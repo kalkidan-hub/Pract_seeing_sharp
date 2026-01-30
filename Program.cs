b0f8a899d09210bf5e6747a0a6685987dda81fd7
kalkidan-hub <kalkidandereje666@gmail.com>
2026-01-30 13:16:24 +0300

    reversing string

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Reverse String
+
+Console.WriteLine("Enter a value you want to see reversed");
+string inputString = Console.ReadLine();
+string reversedString = "";
+
+for (int i = inputString.Length - 1; i >= 0; i--)
+{
+    reversedString += inputString[i];
+}
+Console.WriteLine(reversedString);
