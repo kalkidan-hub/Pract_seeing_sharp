c6241ae2aafeeaba6a4ad35401c8e4d72e27d126
kalkidan-hub <kalkidandereje666@gmail.com>
2026-01-30 14:26:56 +0300

    credit card masker program

---
+++ Program2.cs
@@ -0,0 +1,39 @@
+﻿// a program that masks credit card number
+
+namespace Project4
+{
+    internal class Program2
+    {
+        public static void Main(string[] args)
+        {
+            string ccNum;
+            do
+            {
+                Console.WriteLine("Please enter your credit card");
+                ccNum = Console.ReadLine();
+
+            } while (string.IsNullOrEmpty(ccNum));
+
+            Console.WriteLine(Masker(ccNum));
+        }
+
+        public static string Masker(string creditCard)
+        {
+            string maskedCard = "";
+            for (int i = 0;  i < creditCard.Length; i++)
+            {
+                if (creditCard[i] == '-' || char.IsWhiteSpace(creditCard[i]) || i >= creditCard.Length - 4)
+                {
+                    maskedCard += creditCard[i];
+                }
+                else
+                {
+                    maskedCard += "X";
+                }
+
+            }
+            return maskedCard;
+
+        }
+    }
+}
