a2f3e30d29b81f1e9a65a11f8cbbbcaa00ef2504
kalkidan-hub <kalkidandereje666@gmail.com>
2026-02-13 03:47:18 +0300

    hello world and fizz buzzing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proj2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
